c1f81793cbc726d2638118030e2a06d1bde5f8db
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-14 15:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return `I love you, too.`
+  } else if (string === string.toLowerCase()) {
+    return `I can't hear you!` 
+  } else if (string === string.toUpperCase()) {
+    return `YES INDEED!`
+  }
+}
